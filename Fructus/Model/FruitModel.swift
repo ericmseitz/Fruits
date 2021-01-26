0452d31ab773aacb9e091a7ab8ec52b3e08c4710
Eric M. Seitz <eric.seitz@gmail.com>
2021-01-26 12:33:02 -0800

    onboarding portion of app complete

---
+++ Fructus/Model/FruitModel.swift
@@ -0,0 +1,21 @@
+//
+//  FruitModel.swift
+//  Fructus
+//
+//  Created by Eric Seitz on 1/26/21.
+//
+
+import SwiftUI
+
+// MARK: - FRUITS DATA MODEL
+
+struct Fruit: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String] 
+}
+
