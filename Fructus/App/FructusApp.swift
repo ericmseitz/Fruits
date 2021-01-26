0dd897ab62a5ab945d959bec89619ab7760fc795
Eric M. Seitz <eric.seitz@gmail.com>
2021-01-26 09:54:09 -0800

    Initial commit

---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Eric Seitz on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
