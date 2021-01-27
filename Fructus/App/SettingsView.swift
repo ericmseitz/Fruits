d5d27aa79a2ea641dc340fb3827b2a3dbb2fd22c
Eric M. Seitz <eric.seitz@gmail.com>
2021-01-27 13:17:08 -0800

    Ready to add a settingsView

---
+++ Fructus/App/SettingsView.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsView.swift
+//  Fructus
+//
+//  Created by Eric Seitz on 1/26/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+            .preferredColorScheme(.dark)
+    }
+}
