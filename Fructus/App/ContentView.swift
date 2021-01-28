796b320e0ab38f70e4955ae9daf84ee8358ba5cb
Eric M. Seitz <eric.seitz@gmail.com>
2021-01-28 15:45:35 -0800

    Add settings view

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     
     // MARK: - PROPERTIES
     
+    @State private var isShowingSettings: Bool = false
+    
     var fruits: [Fruit] = fruitsData
     
     // MARK: - BODY
@@ -26,6 +28,15 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Fruits")
+            .navigationBarItems(trailing: Button(action: {
+                isShowingSettings = true
+            }) {
+                Image(systemName: "gearshape")
+                } //: BUTTON
+            .sheet(isPresented: $isShowingSettings) {
+                SettingsView()
+                }
+            )
         } //: NAVIGATION
     }
 }
