b22d39d153c508579290c80fe81e31515061b350
Eric M. Seitz <eric.seitz@gmail.com>
2021-01-30 07:55:14 -0800

    applicaiton complete

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -11,6 +11,7 @@ struct SettingsView: View {
     // MARK: - PROPERTIES
     
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     // MARK: - BODY
     
@@ -35,6 +36,30 @@ struct SettingsView: View {
                     }
 
                     // MARK: - SECTION 2
+                    GroupBox(label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")) {
+                        Divider().padding(.vertical, 4)
+                        Text("If you wish, you can restart Fructus by toggling the switch in this box. That way, when first launched, the appliciation will display the onboarding process.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight((.bold))
+                                    .foregroundColor(Color.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(Color(UIColor.tertiarySystemBackground)
+                                        .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous)))
+                    } //: BOX
                     
                     // MARK: - SECTION 3
                     GroupBox(label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
